8950edc26edc80dce04e62888d5113b3fc7f5ab9
braxton hancock <bdhancock@olivet.edu>
2024-07-20 20:16:26 -0400

    homemade logo. so no accredidation 😁

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ import { AntdRegistry } from "@ant-design/nextjs-registry";
 export const metadata = {
   title: "MoneyManager",
   description: "Show stats and see history better than pnc",
-  icons: [{ rel: "icon", url: "/moneyLogo.png" }],
+  icons: [{ rel: "icon", url: "/money.svg" }],
 };
 
 export default function RootLayout({
