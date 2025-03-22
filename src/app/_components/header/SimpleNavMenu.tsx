ce915c350f0dcacedd09dafc1724b10f094c1de6
Braxton Hancock <braxdhancock@gmail.com>
2025-03-22 15:44:25 -0400

    update

--- src/app/_components/header/SimpleNavMenu.tsx
+++ src/app/_components/header/SimpleNavMenu.tsx
@@ -1,15 +1,22 @@
 "use client";
 
 import Link from "next/link";
+import { Router } from "next/router";
+import { usePathname } from "next/navigation";
 
 const SimpleNavMenu = ({ session }: { session: boolean }) => {
+  const pathname = usePathname();
+
   return (
     <header className="bg-green-800">
       <nav className="flex justify-around">
-        <Link href={"/"} className="">
+        <Link href={pathname != "/" ? "/" : {}} className="">
           Dashboard
         </Link>
-        <Link href={"/transactions"} className="">
+        <Link
+          href={pathname != "/transactions" ? "/transactions" : {}}
+          className=""
+        >
           Transactions
         </Link>
         <Link href={session ? "/api/auth/signout" : "/api/auth/signin"}>
